65d19e9b0c683359695f1610367910d29ee9ca17
914516716@qq.com <914516716@qq.com>
2019-09-21 12:24:08 +0800

    增加了二分解法

--- AboutJava/src/LeetCode/LongestCommonPrefix.java
+++ AboutJava/src/LeetCode/LongestCommonPrefix.java
@@ -29,9 +29,42 @@ public class LongestCommonPrefix {
         return left.substring(0, len);
     }
 
+
+    // 二分法
+    public String longestCommonPrefix(String[] strs, boolean binary) {
+        if (strs.length == 0 || strs == null) return "";
+        else {
+            int minLen = Integer.MAX_VALUE;
+            for (String s : strs)
+                minLen = Math.min(minLen, s.length());  // 找到最短的字符串长度作为二分的上界
+
+            int low = 1;
+            int hight = minLen;
+
+            while (low <= hight) {          // 二分查找最长前缀的长度
+                int mid = (low + hight) / 2;
+                if (isCommonPrefix(strs, mid)) {
+                    low = mid + 1;
+                } else {
+                    hight = mid - 1;
+                }
+            }
+
+            return strs[0].substring(0, hight);
+        }
+    }
+
+    public boolean isCommonPrefix(String[] strs, int pos) {
+        String prefix = strs[0].substring(0, pos);
+        for (String s : strs) {
+            if (!s.startsWith(prefix)) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         LongestCommonPrefix l = new LongestCommonPrefix();
-        String[] s = {"abcde", "abcd", "ab", "cc"};
-        System.out.println(l.longestCommonPrefix(s));
+        String[] s = {"abcde", "abcd", "ab", "abcdef"};
+        System.out.println(l.longestCommonPrefix(s, true));
     }
 }
